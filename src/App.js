dd97b72e6742c87dc0bb75a9902f09df21ed464d
Syed Asil Ali <asilshah17@gmail.com>
2023-06-11 10:35:22 +0500

    Deployed Website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Content from "./Content";
+import Header from "./Header";
+
+
 
 function App() {
+
+  const [student , setStudent] = useState([
+    {
+       id : 1,
+       name: 'Asil',
+       email:'asilshah17@gmail.com',
+       website: 'www.xyz.com',
+       avatar : 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
+      gender : 'male',
+      skills : 'html'
+    }
+  ])
+  const [name , setName] = useState('')
+  const [email , setEmail] = useState('')
+  const [website , setWebsite] = useState('')
+  const [avatar , setAvatar] = useState('')
+  const [gender , setGender] = useState('')
+  const [skills , setSkills] = useState('')
+
+  const handlecheckbox=(value)=>{
+    !skills.length ? (
+      setSkills(value)
+    ): (
+      setSkills(skills.concat(`, ${value}`))
+    )
+  }
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    const id = student.length? student[student.length-1].id + 1 : 1;
+    const newStudent = {id , name , email ,website , avatar , gender, skills}
+    const listStudents = [...student , newStudent];
+    setStudent(listStudents)
+    localStorage.setItem('studentlist' , JSON.stringify(listStudents))
+    setName('')
+    setEmail('')
+    setWebsite('')
+    setAvatar('')
+    setGender('')
+    setSkills('')
+  }
+
+  console.log(student);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header 
+        title = {'Student Enrollment Form'}/>
+      <Content
+        student = {student}
+        setStudent={setStudent}
+        name={name}
+        setName={setName}
+        email={email}
+        setEmail={setEmail}
+        website={website}
+        setWebsite={setWebsite}
+        avatar={avatar}
+        setAvatar={setAvatar}
+        gender={gender}
+        setGender={setGender}
+        skills={skills}
+        setSkills={setSkills}
+        handlecheckbox = {handlecheckbox}
+        handleSubmit = {handleSubmit}
+        />
     </div>
   );
 }
